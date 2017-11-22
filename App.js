1c2a22baf1b3c45dff45d56a67ec57283f1ef262
Jason Crawford <daxapps777@gmail.com>
2017-11-21 20:21:15 -0600

    Enable user sign in with code from text

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import SignUpForm from './components/SignUpForm';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import firebase from "firebase";
+import SignUpForm from "./components/SignUpForm";
+import SignInForm from "./components/SignInForm";
 
 export default class App extends React.Component {
+  componentDidMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: "AIzaSyCqVyvLfGBkWZSzuXJ2DiywVoXYjySiPP4",
+      authDomain: "one-time-password-10b1e.firebaseapp.com",
+      databaseURL: "https://one-time-password-10b1e.firebaseio.com",
+      projectId: "one-time-password-10b1e",
+      storageBucket: "one-time-password-10b1e.appspot.com",
+      messagingSenderId: "718516661008"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
@@ -15,8 +31,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-around"
+  }
 });
--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { FormLabel, FormInput, Button } from 'react-native-elements';
-import axios from 'axios';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { FormLabel, FormInput, Button } from "react-native-elements";
+import axios from "axios";
 
-const ROOT_URL = 'https://us-central1-one-time-password-10b1e.cloudfunctions.net';
+const ROOT_URL =
+	"https://us-central1-one-time-password-10b1e.cloudfunctions.net";
 
 class SignUpForm extends Component {
-	state = { phone: ''};
+	state = { phone: "" };
 
 	handleSubmit = async () => {
 		try {
-			await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone });			
-			await axios.post(`${ROOT_URL}/requireOneTimePassword`, { phone: this.state.phone });
+			await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone });
+			await axios.post(`${ROOT_URL}/requireOneTimePassword`, {
+				phone: this.state.phone
+			});
 		} catch (err) {
 			console.log(err);
 		}
-	}
+	};
 
 	render() {
 		return (
 			<View>
 				<View style={{ marginBottom: 10 }}>
 					<FormLabel>Enter Phone Number</FormLabel>
-					<FormInput 
+					<FormInput
 						value={this.state.phone}
 						onChangeText={phone => this.setState({ phone })}
 					/>
